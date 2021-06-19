fc95aaad432357eb62848a06e96c1de7244e1e1d
Kevin Garner <kevin@kgar.net>
2021-06-18 21:59:23 -0500

    added a lot of additional token scanning

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,6 +1,7 @@
 
-use super::Token;
+use crate::Token;
 use crate::TokenType;
+use crate::Literal;
 
 pub struct Scanner {
     source: String,
@@ -28,6 +29,7 @@ impl Scanner {
         }
 
         self.tokens.push(Token::new(TokenType::Eof, "".to_string(), None, self.line));
+
         self.tokens.clone()
     }
 
@@ -35,7 +37,77 @@ impl Scanner {
         self.current >= self.source.len()
     }
 
-    fn scan_token(&self) {
+    fn scan_token(&mut self) {
+        let ch: char = self.advance();
+        match ch {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            '!' => {
+                if self.match_next('=') {
+                    self.add_token(TokenType::BangEqual)
+                } else {
+                    self.add_token(TokenType::Bang)
+                }
+            }
+            '=' => {
+                if self.match_next('=') {
+                    self.add_token(TokenType::EqualEqual)
+                } else {
+                    self.add_token(TokenType::Equal)
+                }
+            }
+            '<' => {
+                if self.match_next('=') {
+                    self.add_token(TokenType::LessEqual)
+                } else {
+                    self.add_token(TokenType::Less)
+                }
+            }
+            '>' => {
+                if self.match_next('=') {
+                    self.add_token(TokenType::GreaterEqual)
+                } else {
+                    self.add_token(TokenType::Greater)
+                }
+            }
+            _ => crate::error(self.line, "Unexpected token".to_string()),
+        }
+    }
 
+    fn advance(&mut self) -> char {
+        let ch = self.source.chars().nth(self.current).expect("Error in advance");
+        self.current += 1;
+        ch
+    }
+
+    fn add_token(&mut self, t: TokenType) {
+        self.add_token_literal(t, None);
+    }
+
+    fn add_token_literal(&mut self, ty: TokenType, lit: Option<Literal>) {
+        let text = &self.source[self.start..=self.current];
+        self.tokens.push(Token::new(ty, text.to_string(), lit, self.line));
+    }
+
+    fn match_next(&mut self, expect: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+
+        if self.source.chars().nth(self.current).expect("Error in match_next") != expect {
+            return false;
+        }
+
+        self.current += 1;
+
+        true
     }
 }
