65d258c15da32f2a48bdc71c210e1577fba7b0c8
Kevin Garner <kevin@kgar.net>
2021-07-17 18:42:46 -0500

    more work on parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,8 @@
 // parser
 
 use crate::Token;
+use crate::TokenType;
+use crate::Expr;
 
 pub struct Parser {
     tokens: Vec<Token>,
@@ -14,3 +16,45 @@ impl Parser {
             current: 0
         }
     }
+
+    fn expression() -> Expr {
+        equality()
+    }
+
+    fn equality(&self) -> Expr {
+        let mut expr = comparison();
+
+        while match_tokens() {
+            let oper = previous();
+            let r = comparison();
+        }
+    }
+
+    fn check(&self, ttype: TokenType) -> bool {
+        if is_at_end() {
+            false
+        } else {
+            peek().type == ttype
+        }
+    }
+
+    fn advance(&self) -> Token {
+        if !is_at_end() {
+            self.current++;
+        }
+
+        previous()
+    }
+
+    fn is_at_end() -> bool {
+        peek().type == TokenType::Eof;
+    }
+
+    fn peek(&self) -> Token {
+        self.tokens.get(current)
+    }
+
+    fn previous(&self) -> Token {
+        self.tokens.get(current - 1)
+    }
+}
